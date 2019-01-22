1aef8e4be7b0651ef1b6dcf044acff42e51d8bbb
crzdieu <bryanwp88@gmail.com>
2019-01-22 09:17:52 +0100

    exercice6

---
+++ javascript.js
@@ -0,0 +1,6 @@
+function toggle(){
+var nombre1 = document.getElementById('firstNumber').value;
+var nombre2 = document.getElementById('secondNumber').value;
+var result = nombre1%nombre2;
+alert(result);
+            }
